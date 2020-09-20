1a4058547101fcd6a8159ded0019c00ca6cdc4c7
Luigi Grilli <luigi.grilli@gmail.com>
2020-09-20 10:09:52 +0100

    Added json project

---
+++ src/Grillisoft.Configuration.Json/JsonValueStore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Grillisoft.Configuration.Stores;
+
+namespace Grillisoft.Configuration.Json
+{
+    public class JsonValueStore : IValuesStore
+    {
+        private readonly MemoryValuesStore _store;
+
+        public JsonValueStore()
+            : this(new MemoryValuesStore())
+        {
+        }
+
+        private JsonValueStore(MemoryValuesStore store)
+        {
+            _store = store;
+        }
+
+        public bool IsSafe => false;
+
+        public bool TryGetValue(string key, out string value)
+        {
+            return _store.TryGetValue(key, out value);
+        }
+
+    }
+}
